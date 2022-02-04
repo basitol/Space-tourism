7437cab331655c2065cb2e80dfd6f9688b222ac4
basitol <basitolaitan27@gmail.com>
2022-02-04 12:29:01 +0100

    Adding react packages

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <h2>This is nav</h2>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
